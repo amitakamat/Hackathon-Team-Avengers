da421d6571f34d1227510f74768e55d5045c2615
Mohammed Haroon Shareef <shareefaqyar@gmail.com>
2017-11-17 22:41:14 -0800

    updated mongodb cluster

--- LinkRedirect/appGET.js
+++ LinkRedirect/appGET.js
@@ -62,7 +62,7 @@ console.log("lets check"+shortUrl);
      });
   
 
-  MongoClient.connect("mongodb://admin:admin@281-hackathon-shard-00-00-i3eln.mongodb.net:27017,281-hackathon-shard-00-01-i3eln.mongodb.net:27017,281-hackathon-shard-00-02-i3eln.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
+  MongoClient.connect("mongodb://haroon:haroon@281-hackathon-shard-00-00-pjkz1.mongodb.net:27017,281-hackathon-shard-00-01-pjkz1.mongodb.net:27017,281-hackathon-shard-00-02-pjkz1.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
   if(err) { return console.dir(err); }
   console.log("Updating STATS!!")
   connection.query("SELECT websitename from urlinfo where shorturl='"+shortUrl+"'", function(err, rows, fields) {
@@ -131,7 +131,7 @@ app.get('/', function (req, res) {
 
 app.get('v1/hits/:keys', function(req, res) {
   console.log("Get hits"+req.params.keys);
-   MongoClient.connect("mongodb://admin:admin@281-hackathon-shard-00-00-i3eln.mongodb.net:27017,281-hackathon-shard-00-01-i3eln.mongodb.net:27017,281-hackathon-shard-00-02-i3eln.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
+   MongoClient.connect("mongodb://haroon:haroon@281-hackathon-shard-00-00-pjkz1.mongodb.net:27017,281-hackathon-shard-00-01-pjkz1.mongodb.net:27017,281-hackathon-shard-00-02-pjkz1.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
     //MongoClient.connect("mongodb://localhost:27017/stats", function(err,db) { 
     var collection = db.collection('webstats_hits');
     collection.findOne({key:req.params.keys},{},function(e,docs){
@@ -144,7 +144,7 @@ app.get('v1/hits/:keys', function(req, res) {
 app.get('/v1/domain/:domain', function(req, res) {
   var domain_name = req.params.domain;
   console.log("Get domain hits for : "+ domain_name);
-  MongoClient.connect("mongodb://admin:admin@281-hackathon-shard-00-00-i3eln.mongodb.net:27017,281-hackathon-shard-00-01-i3eln.mongodb.net:27017,281-hackathon-shard-00-02-i3eln.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
+  MongoClient.connect("mongodb://haroon:haroon@281-hackathon-shard-00-00-pjkz1.mongodb.net:27017,281-hackathon-shard-00-01-pjkz1.mongodb.net:27017,281-hackathon-shard-00-02-pjkz1.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
     //MongoClient.connect("mongodb://localhost:27017/stats", function(err,db) { 
     if(err){
       console.log("error is "+err);
@@ -158,7 +158,7 @@ app.get('/v1/domain/:domain', function(req, res) {
 
 app.get('/v1/domain', function(req,res){
 console.log("Get all domain names")
-  MongoClient.connect("mongodb://admin:admin@281-hackathon-shard-00-00-i3eln.mongodb.net:27017,281-hackathon-shard-00-01-i3eln.mongodb.net:27017,281-hackathon-shard-00-02-i3eln.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
+  MongoClient.connect("mongodb://haroon:haroon@281-hackathon-shard-00-00-pjkz1.mongodb.net:27017,281-hackathon-shard-00-01-pjkz1.mongodb.net:27017,281-hackathon-shard-00-02-pjkz1.mongodb.net:27017/stats?ssl=true&replicaSet=281-hackathon-shard-0&authSource=admin", function(err, db) {
     //MongoClient.connect("mongodb://localhost:27017/stats", function(err,db) { 
     if(err){
       console.log("error is "+err);
